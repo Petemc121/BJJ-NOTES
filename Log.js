ec46896358b1b0a7c2c339c5e42a717bb810fa08
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-06-18 19:45:06 +0900

    Update Log.js
    Added a Notes component

--- Log.js
+++ Log.js
@@ -1,29 +1,34 @@
-import React, {useRef} from 'react'
+import React, {useState, useRef} from 'react'
+import Notes from './Notes.js'
 
 export default function Log({log}) {
     
-    const note1Ref = useRef();
-    const note2Ref = useRef();
-    const note3Ref = useRef();
     const save = useRef();
     const cancel = useRef();
     const con = useRef();
+    let noteId = 0;
+    const [notes, setNotes] = useState([])
     function edit() {
         con.current.style.display ="flex"
         save.current.style.display ="block"
         cancel.current.style.display ="block"
-        note1Ref.current.style.display ="block"
-        note2Ref.current.style.display ="block"
-        note3Ref.current.style.display ="block"
+     
     }
 
     function cancelled() {
         con.current.style.display ="none"
         save.current.style.display ="none"
         cancel.current.style.display ="none"
-        note1Ref.current.style.display ="none"
-        note2Ref.current.style.display ="none"
-        note3Ref.current.style.display ="none"
+
+    }
+
+    function saved() {
+        
+
+        setNotes(notes => {
+            noteId++;
+            return [...notes, {noteText:"", noteID:noteId}]
+        })
     }
 
    
@@ -37,9 +42,8 @@ export default function Log({log}) {
             {log.technique}
 
             <h4>Notes</h4>
-            1: <input ref={note1Ref} class="notesIn" id="note1In"></input><div class="notesOut" id="note1Out">{log.note1}</div>
-            2: <input ref={note2Ref} class="notesIn" id="note2In"></input><div class="notesOut" id="note2Out">{log.note2}</div>
-            3: <input ref={note3Ref} class="notesIn" id="note3In"></input><div class="notesOut" id="note3Out">{log.note3}</div>
+
+            <Notes notes={notes}/>
 
             <div ref={con} id="saveCancelCon">
             <button ref={save} onClick={saved} class="saveCancel">Save</button>
