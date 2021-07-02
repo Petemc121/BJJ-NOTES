0872389ab3f19eb5177dc67a30ed24c3af9ca941
Petemc121 <petemc121@gmail.com>
2021-07-02 16:27:05 +0900

    further explored drag and drop functionality

--- BJJLogs.js
+++ BJJLogs.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Log from './Log'
 
 export default function BJJLogs({logs, handleDeleteLog}) {
-
+    function handleDragStart(e) {
+        this.style.opacity = '0.4';  // this / e.target is the source node.
+      }
 
     return (
        
         <div id="bjjLogsCon">
             
            {logs.map(log => {
+
                return <Log key={log.id} handleDeleteLog={handleDeleteLog} log={log} />
            })}
            
--- Log.js
+++ Log.js
@@ -20,11 +20,15 @@ export default function Log({log, handleDeleteLog}) {
 
     }
 
+    function handleDragStart(e) {
+        e.style.opacity = '0.4';  // this / e.target is the source node.
+      }
+
    
 
 
     return (
-        <div draggable="true" style={{backgroundColor: log.color}} class="log">
+        <div draggable="true" dragStart={handleDragStart}  style={{backgroundColor: log.color}} class="log">
            <h4>Technique</h4>
             {log.technique}
 
